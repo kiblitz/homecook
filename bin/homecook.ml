a4e85911e835aa7929c78bbbe2900bd25bd94e10
Gabriel Lee <gelos@localhost.localdomain>
2026-01-06 15:45:58 -0500

    feature -- add squares

--- bin/homecook.ml
+++ bin/homecook.ml
@@ -37,31 +37,142 @@ module S =
 
       .light-square { background-color: #eeeed2; }
       .dark-square { background-color: #769656; }
+
+      .circle {
+        width: 100%;
+        height: 100%;
+        border-radius: 50%;
+        background-color: rgba(0, 0, 0, 0.5);
+      }
     |}]
 
-let chessboard_component graph =
-  let range = List.range 0 8 |> Int.Set.of_list |> return in
+(*
+let draggable_piece =
+  Bonsai_web_ui_drag_and_drop.create
+    ~source_id:(module Int)
+    ~target_id:(module Int)
+    ~on_drop:((fun _source_id _target_id -> Ui_effect.Ignore) |> return)
+;;
+*)
+
+module File = struct
+  module T : sig
+    type t [@@deriving compare, sexp_of]
+
+    val to_string : t -> string
+    val of_idx_exn : int -> t
+    val of_char_exn : char -> t
+    val to_idx : t -> int
+  end = struct
+    type t = char [@@deriving compare, sexp_of]
+
+    let to_string = Char.to_string
+    let is_valid_char c = Char.('A' <= c && c <= 'Z')
+
+    let of_idx_exn i =
+      let alpha_adjusted_i = i + Char.to_int 'A' in
+      match Char.of_int alpha_adjusted_i with
+      | Some c when is_valid_char c -> c
+      | Some c -> raise_s [%message "max 26 files" (c : char)]
+      | None ->
+        raise_s [%message "char encoding out of range" (i : int) (alpha_adjusted_i : int)]
+    ;;
+
+    let to_idx t = Char.to_int t - Char.to_int 'A'
+
+    let of_char_exn t =
+      if is_valid_char t
+      then t
+      else raise_s [%message "Invalid char, expected capital alpha" (t : char)]
+    ;;
+  end
+
+  include T
+  include Comparable.Make_plain (T)
+end
+
+module Rank = struct
+  module T : sig
+    type t [@@deriving compare, sexp_of]
+
+    val to_string : t -> string
+    val of_idx : int -> t
+    val to_idx : t -> int
+    val of_int : int -> t
+  end = struct
+    type t = int [@@deriving compare, sexp_of]
+
+    let to_string = Int.to_string
+    let of_idx i = i + 1
+    let to_idx t = t - 1
+    let of_int = Fn.id
+  end
+
+  include T
+  include Comparable.Make_plain (T)
+end
+
+let chessboard_component ?(width = 8) ?(height = 8) graph =
+  let files =
+    List.range 0 width |> List.map ~f:File.of_idx_exn |> File.Set.of_list |> return
+  in
+  let ranks =
+    List.range 0 height |> List.map ~f:Rank.of_idx |> Rank.Set.of_list |> return
+  in
   let grid =
     Bonsai.assoc_set
-      (module Int)
-      range
+      (module File)
+      files
       graph
-      ~f:(fun rank graph ->
+      ~f:(fun file graph ->
         Bonsai.assoc_set
-          (module Int)
-          range
+          (module Rank)
+          ranks
           graph
-          ~f:(fun file _graph ->
+          ~f:(fun rank _graph ->
             let%arr rank = rank
             and file = file in
-            let is_light = (rank + file) % 2 = 0 in
+            let is_light = (Rank.to_idx rank + File.to_idx file) % 2 = 0 in
+            let piece =
+              if
+                [%equal: File.t] file (File.of_char_exn 'E')
+                && [%equal: Rank.t] rank (Rank.of_int 4)
+              then Some (Vdom.Node.div ~attrs:[ S.circle; Vdom.Attr.draggable true ] [])
+              else None
+            in
             Vdom.Node.div
-              ~attrs:[ (if is_light then S.light_square else S.dark_square) ]
-              []))
+              ~attrs:
+                [ (if is_light then S.light_square else S.dark_square)
+                ; Vdom.Attr.create "coord" [%string "%{file#File}%{rank#Rank}"]
+                ; [ Css_gen.create
+                      ~field:"grid-row"
+                      ~value:[%string "%{height - Rank.to_idx rank#Int}"]
+                  ; Css_gen.create
+                      ~field:"grid-column"
+                      ~value:[%string "%{File.to_idx file + 1#Int}"]
+                  ]
+                  |> Css_gen.concat
+                  |> Vdom.Attr.style
+                ]
+              (Option.to_list piece)))
   in
   let%arr grid = grid in
   let squares = Map.data grid |> List.map ~f:Map.data |> List.concat in
-  Vdom.Node.div ~attrs:[ S.board ] squares
+  Vdom.Node.div
+    ~attrs:
+      [ [ Css_gen.display `Inline_grid
+        ; Css_gen.create
+            ~field:"grid-template-columns"
+            ~value:[%string "repeat(%{width#Int},5rem)"]
+        ; Css_gen.create
+            ~field:"grid-template-rows"
+            ~value:[%string "repeat(%{height#Int},5rem)"]
+        ; Css_gen.border ~width:(`Px 2) ~color:(`Hex "#333") ~style:`Solid ()
+        ]
+        |> Css_gen.concat
+        |> Vdom.Attr.style
+      ]
+    squares
 ;;
 
 let item ~index:_ ~source _which _data graph =
